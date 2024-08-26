98f07a145794ca7fd77e3204b71d7b2ef5f7024f
bororo15 <ppeuang795@gmail.com>
2024-08-26 17:02:27 +0900

    Add MyBatis and Lombok configuration, create DTO classes
    - Configure MyBatis in application.properties
    - Add Lombok dependency and annotation processor
    - Create User-related DTO classes
    - Update UserController to use DTO classes

---
+++ src/main/java/com/example/project_ex1/dto/UserIdRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.project_ex1.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserIdRequestDto {
+    private long userId;
+}
--- src/main/java/com/example/project_ex1/dto/UserUpdateRequestDto.java
+++ src/main/java/com/example/project_ex1/dto/UserUpdateRequestDto.java
@@ -1,17 +1,14 @@
-package com.example.project_ex1.model;
+package com.example.project_ex1.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
-    private Long userId;
+public class UserUpdateRequestDto {
     private String userName;
     private String userEmail;
     private String userPhoneNumber;
-    private LocalDateTime regDt;
 }
